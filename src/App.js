44e5fc3bf2f740f6acfd1dc032828d88326c82b7
USAMA <usamaahmadshah06@gmail.com>
2022-08-20 15:35:09 +0500

    blog_frontendonly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticlesList from './pages/ArticlesList';
+import Navbar from './components/Navbar';
+import NotFound from './pages/NotFound';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="max-w-screen-md mx-auto pt-20">
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/article/:name' element={<Article/>}/>
+        <Route path='/articlelist' element={<ArticlesList/>}/>
+        <Route path='*' element={<NotFound/>}/>
+       </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,83 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello  welcome to my blog.</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Maecenas imperdiet scelerisque porttitor.
+            Mauris pulvinar sollicitudin leo, ut ultricies est vestibulum venenatis.
+            Vestibulum vitae turpis pulvinar, elementum ligula et, rhoncus neque.
+            Etiam quam tortor, convallis eu pellentesque ut, aliquam vel erat.
+            Quisque sed dui at leo iaculis consequat.
+            Donec vitae velit sodales, tristique odio ac, euismod arcu.
+            Praesent vitae nisi erat.
+            Phasellus eget vehicula nibh.
+            Quisque pulvinar turpis id eros faucibus, finibus fringilla libero pulvinar.
+            Praesent fermentum ipsum eu scelerisque viverra. 
+            Proin dictum scelerisque ex aliquam posuere. 
+            Phasellus quis placerat metus. Maecenas fringilla id justo quis sagittis. 
+            Sed malesuada laoreet lacus vel commodo. 
+            Integer malesuada sem ut tellus tempus tincidunt.
+            Nullam consequat, magna nec posuere varius, leo libero commodo mi, non tristique nisi ipsum ut dolor.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Maecenas imperdiet scelerisque porttitor.
+            Mauris pulvinar sollicitudin leo, ut ultricies est vestibulum venenatis.
+            Vestibulum vitae turpis pulvinar, elementum ligula et, rhoncus neque.
+            Etiam quam tortor, convallis eu pellentesque ut, aliquam vel erat.
+            Quisque sed dui at leo iaculis consequat.
+            Donec vitae velit sodales, tristique odio ac, euismod arcu.
+            Praesent vitae nisi erat.
+            Phasellus eget vehicula nibh.
+            Quisque pulvinar turpis id eros faucibus, finibus fringilla libero pulvinar.
+            Praesent fermentum ipsum eu scelerisque viverra. 
+            Proin dictum scelerisque ex aliquam posuere. 
+            Phasellus quis placerat metus. Maecenas fringilla id justo quis sagittis. 
+            Sed malesuada laoreet lacus vel commodo. 
+            Integer malesuada sem ut tellus tempus tincidunt.
+            Nullam consequat, magna nec posuere varius, leo libero commodo mi, non tristique nisi ipsum ut dolor.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Maecenas imperdiet scelerisque porttitor.
+            Mauris pulvinar sollicitudin leo, ut ultricies est vestibulum venenatis.
+            Vestibulum vitae turpis pulvinar, elementum ligula et, rhoncus neque.
+            Etiam quam tortor, convallis eu pellentesque ut, aliquam vel erat.
+            Quisque sed dui at leo iaculis consequat.
+            Donec vitae velit sodales, tristique odio ac, euismod arcu.
+            Praesent vitae nisi erat.
+            Phasellus eget vehicula nibh.
+            Quisque pulvinar turpis id eros faucibus, finibus fringilla libero pulvinar.
+            Praesent fermentum ipsum eu scelerisque viverra. 
+            Proin dictum scelerisque ex aliquam posuere. 
+            Phasellus quis placerat metus. Maecenas fringilla id justo quis sagittis. 
+            Sed malesuada laoreet lacus vel commodo. 
+            Integer malesuada sem ut tellus tempus tincidunt.
+            Nullam consequat, magna nec posuere varius, leo libero commodo mi, non tristique nisi ipsum ut dolor.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Maecenas imperdiet scelerisque porttitor.
+            Mauris pulvinar sollicitudin leo, ut ultricies est vestibulum venenatis.
+            Vestibulum vitae turpis pulvinar, elementum ligula et, rhoncus neque.
+            Etiam quam tortor, convallis eu pellentesque ut, aliquam vel erat.
+            Quisque sed dui at leo iaculis consequat.
+            Donec vitae velit sodales, tristique odio ac, euismod arcu.
+            Praesent vitae nisi erat.
+            Phasellus eget vehicula nibh.
+            Quisque pulvinar turpis id eros faucibus, finibus fringilla libero pulvinar.
+            Praesent fermentum ipsum eu scelerisque viverra. 
+            Proin dictum scelerisque ex aliquam posuere. 
+            Phasellus quis placerat metus. Maecenas fringilla id justo quis sagittis. 
+            Sed malesuada laoreet lacus vel commodo. 
+            Integer malesuada sem ut tellus tempus tincidunt.
+            Nullam consequat, magna nec posuere varius, leo libero commodo mi, non tristique nisi ipsum ut dolor.
+        </p>
+    </div>
+  );
+}
+
+export default Home
